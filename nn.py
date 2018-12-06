0c9ae99482b0f54831ff8259d43fe1aad23e23cf
noyesa@myumanitoba.ca <aaronnoyes@Aarons-MacBook-Pro.local>
2018-12-06 16:48:49 -0600

    refactored summary to it's own function and cleaned up comments

--- nn.py
+++ nn.py
@@ -7,7 +7,7 @@ import pdb
 import matplotlib.pyplot as plt
 
 class NeuralNetwork:
-    def __init__(self, x, y, activation='sigmoid', errorFunction='mse', learningRate=0.01):
+    def __init__(self, x, y, activation='sigmoid', errorFunction='mse', learningRate=0.01, goalError=0.002, trialLimit=3000):
         self.input      = x
         self.weights1   = np.random.rand(self.input.shape[1],4)
         self.weights2   = np.random.rand(4,1)
@@ -18,6 +18,8 @@ class NeuralNetwork:
         self.history = []
         self.learningRate = learningRate
         self.trials = 0
+        self.goalError = goalError
+        self.trialLimit = trialLimit
 
     def sigmoid(self, x, derivative=False):
         if derivative:
@@ -68,20 +70,15 @@ class NeuralNetwork:
         self.backprop()
 
     def learn(self):
-        # for i in range(3000):
-        #     self.train()
-        while self.calculateError() > 0.002:
+        while self.calculateError() > self.goalError:
             self.trials += 1
             self.train()
-            if self.trials > 3000:
-                print("Timed out at 3000 trials")
+            if self.trials > self.trialLimit:
+                print("Timed out after " + str(self.trialLimit) + " trials")
                 break
 
     def summary(self):
-        plt.plot(self.history, label=self.activation)
-        plt.xlabel('trials')
-        plt.ylabel('mean squared error')
-        plt.legend()
+
         print (self.activation)
         print ("Input : \n" + str(self.input))
         print ("Actual Output : \n" + str(self.y))
@@ -89,19 +86,55 @@ class NeuralNetwork:
         print ("Mean Squared Error: " + str(self.calculateError()))
         print ("Trials Required: " + str(self.trials))
         print ("\n")
-        # plt.show()
+
+def compareNetworks(networks):
+    length = len(networks)
+    trials = []
+    activations = []
+    plotColors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd'] #used to match first sublpot colors
+
+    #graph lines as errors change
+    plt.xlabel('trials')
+    plt.ylabel('mean squared error')
+    for nn in networks:
+        plt.subplot(211)
+        plt.xlabel('trials')
+        plt.ylabel('mean squared error')
+        plt.plot(nn.history, label=nn.activation)
+        trials.append(nn.trials)
+        activations.append(nn.activation)
+        nn.summary()
+    plt.legend()
+
+    #bar chart for activation trials required
+    plt.subplot(212)
+    plt.xlabel('trials')
+    plt.legend()
+    plt.bar(np.arange(len(networks)), trials, align='center', color=plotColors)
+    plt.xticks(np.arange(len(networks)), activations)
+
+    #clean up spacing between subplots
+    plt.tight_layout()
+    plt.show()
 
 def main():
+    #make numpy print same precision for all values
     np.set_printoptions(formatter={'float': '{: 0.5f}'.format}) #show trailing 0s and always 5 points of precision for comparison
-    x = np.array(([0,0,1],[0,1,1],[1,0,1],[1,1,1]), dtype=float)
-    y = np.array(([0],[1],[1],[0]), dtype=float)
-    # nn = NeuralNetwork(x, y)
+
+    x = np.array(([0,0,1],[0,1,1],[1,0,1],[1,1,1]), dtype=float) #nn inputs
+    y = np.array(([0],[1],[1],[0]), dtype=float) #expected outputs
+    networks = [] #hold all neural networks for comparison
+
+    #sigmoid network
     NN = NeuralNetwork(x,y, activation='sigmoid', learningRate=1)
     NN.learn()
-    NN.summary()
+    networks.append(NN)
+
+    #relu network
     NN2 = NeuralNetwork(x,y, activation='relu', learningRate=0.01)
     NN2.learn()
-    NN2.summary()
-    plt.show()
+    networks.append(NN2)
+    
+    compareNetworks(networks)
 
 main()
